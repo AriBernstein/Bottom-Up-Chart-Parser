942ead627866a64cc05a76f888262d1dc4f2135e
Ari Bernstein <aribernstein1997@gmail.com>
2021-10-08 15:13:19 -0400

    filled in active arc extension with my best understanding

--- chart/bottom_up_parser.py
+++ chart/bottom_up_parser.py
@@ -21,19 +21,31 @@ def _generate_arcs(agenda:Agenda, chart:Chart) -> None:
         else:   # Add A to chart as incomplete arc
             chart.add_incomplete_arc(new_arc)
         
-    # Active Arc extension - find active arcs with expected_pos equal to this_arc.pos . Search in
-    # range of incomplete arcs with end index of 1 + this_arc.end_index to this_arc.start_index
-    for sentence_index in reversed(range(this_arc.start_index(), this_arc.end_index() + 1)):
-           active_arcs = chart.incomplete_starts[sentence_index]
-           for active_arc in active_arcs:
-               # Moving through an incomplete arc from last to first element in subsequence, check:
-               # 1. if start location of last-added complete arc in subsequence is immediately after
-               #    end location of this_arc
-               # 2. if active_arc is looking for a complete arc of the same pos as this_arc
-               if active_arc.start_index() == this_arc.end_index() + 1 \
-                   and active_arc.expected_pos() == this_arc.get_pos():
-                   pass
+    # # Active Arc extension - find active arcs with expected_pos equal to this_arc.pos . Search in
+    # # range of incomplete arcs with end index of 1 + this_arc.end_index to this_arc.start_index
+    # for sentence_index in reversed(range(this_arc.start_index(), this_arc.end_index() + 1)):
+    #        active_arcs = chart.incomplete_starts[sentence_index]
+    #        for active_arc in active_arcs:
+    #            # Moving through an incomplete arc from last to first element in subsequence, check:
+    #            # 1. if start location of last-added complete arc in subsequence is immediately after
+    #            #    end location of this_arc
+    #            # 2. if active_arc is looking for a complete arc of the same pos as this_arc
+    #            if active_arc.start_index() == this_arc.end_index() + 1 \
+    #                and active_arc.expected_pos() == this_arc.get_pos():
+    #                pass
     
+    # Active Arc extension - find active arcs whose last-added complete arc starts immediately after
+    #                        this_arc ends. Check if they are expecting a new start of the same pos
+    #                        as current_arc and, if so, add current_arc to inactive arc
+    for active_arc in chart.incomplete_starts[this_arc.end_index() + 1].copy():
+        if active_arc.expected_pos() == this_arc.pos():
+            # Update active arc
+            old_start = active_arc.start_index()
+            active_arc.add_to_subsequence(this_arc) # Start is updated here
+            
+            # Update active_arc location within chart
+            chart.update_incomplete_arc(active_arc, old_start)
+            
     # Arc completion
     for newly_completed_arc in newly_completed_arcs:
         agenda.enqueue(newly_completed_arc)
